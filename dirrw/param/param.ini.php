07f9a908c1de3f662c7f7ebc9e128786e7f93c99
delasalle-sio-jim <jean.michel.cartron@gmail.com>
2021-05-19 22:45:24 +0200

    Modification des paramètres d'accès à la BDD

--- dirrw/param/param.ini.php
+++ dirrw/param/param.ini.php
@@ -1,11 +1,23 @@
 <?php
-$nomServeur="mysql51-46.perso";
-$loginAdminServeur="lyceedelasalle";
-$motPasseAdminServeur="patwcg35";
-$nomBaseDonnee="lyceedelasalle";
+// paramètres pour localhost
+$nomServeur="localhost";
+$loginAdminServeur="root";
+$motPasseAdminServeur="";
+$nomBaseDonnee="portefeuille";
 $prefixeTable="port_";
 $crypte=false;
 $smtp=false;
 $melContact="cartron.j@lyceedelasalle.fr";
 $lngmdp=6;
+
+// paramètres pour OVH
+// $nomServeur="mysql51-46.perso";
+// $loginAdminServeur="lyceedelasalle";
+// $motPasseAdminServeur="patwcg35";
+// $nomBaseDonnee="lyceedelasalle";
+// $prefixeTable="port_";
+// $crypte=false;
+// $smtp=false;
+// $melContact="cartron.j@lyceedelasalle.fr";
+// $lngmdp=6;
 ?>
